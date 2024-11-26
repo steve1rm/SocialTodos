398e2939215acd5f4a29a5c84357bf21e302d6d1
Steve <steve@kitkat@gmail.com>
2024-11-26 11:39:35 +0700

    Added some cosmetics to the list screen

--- Todo/src/main/java/me/androidbox/todo/presentation/TodoListScreen.kt
+++ Todo/src/main/java/me/androidbox/todo/presentation/TodoListScreen.kt
@@ -5,19 +5,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.androidbox.todo.domain.models.TodoModel
 
 @Composable
@@ -26,7 +33,8 @@ fun TodoListScreen(
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize().padding(horizontal = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(
             items = todoModel,
@@ -48,34 +56,46 @@ fun TodoItem(
     modifier: Modifier = Modifier
 ) {
 
-    Column(
-        modifier = modifier.fillMaxWidth()
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp)
         ) {
-            Text(text = "User ID: ${todoModel.userId}")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "User ID: ${todoModel.userId}")
 
-            Switch(
-                checked = todoModel.completed,
-                onCheckedChange = { isCompleted ->
-                    /** Update local DB */
-                }
-            )
-        }
+                Switch(
+                    checked = todoModel.completed,
+                    onCheckedChange = { isCompleted ->
+                        /** Update local DB */
+                    }
+                )
+            }
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "User ID: ${todoModel.title}")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = "Title: ${todoModel.title}",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis)
 
-            Icon(
-                imageVector = if(todoModel.completed) Icons.Default.Done else Icons.Default.Close,
-                contentDescription = null,
-                tint = if(todoModel.completed) Color.Green else Color.Red
-            )
+                Icon(
+                    imageVector = if (todoModel.completed) Icons.Default.Done else Icons.Default.Close,
+                    contentDescription = null,
+                    tint = if (todoModel.completed) Color.Green else Color.Red
+                )
+            }
         }
     }
 }
--- Todo/src/main/java/me/androidbox/todo/presentation/TodoListViewModel.kt
+++ Todo/src/main/java/me/androidbox/todo/presentation/TodoListViewModel.kt
@@ -3,7 +3,10 @@ package me.androidbox.todo.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import me.androidbox.todo.domain.models.TodoModel
@@ -17,8 +20,17 @@ class TodoListViewModel(
 
     private val _todoListState = MutableStateFlow(emptyList<TodoModel>())
     val todoListState = _todoListState.asStateFlow()
+        .onStart {
+            fetchLocalTodoItems()
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
 
-    init {
+
+    fun fetchLocalTodoItems() {
         viewModelScope.launch {
             try {
                 fetchLocalTodoUseCase.execute()
